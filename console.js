f476794ba0ddba098310be3370f2cfa08aa62c73
QQ喵 <gqqnb2005@gmail.com>
2021-06-10 16:07:51 +0800

    Clean up

--- console.js
+++ console.js
@@ -1,4 +1,4 @@
-Add=require("./build/contracts/Add.json")
+Add=require("./build/contracts/Add.json");
 
 module.exports = function(callback) {
 	let abi=Add['abi'];
@@ -7,7 +7,14 @@ module.exports = function(callback) {
 	web3.eth.personal.getAccounts().then(accounts=> {
 		let account0=accounts[0];
 
-		c = new web3.eth.Contract(j);
-		c.deploy({data:bytecode, arguments:[4]}).send({from: account0, gas:1500000, gasPrice: '30000000000000'});
+		let c = new web3.eth.Contract(abi);
+		c.deploy({data:bytecode, arguments:[4]})
+			.send({from: account0, gas:1500000, gasPrice: '30000000000000'})
+			.then(function(newContractInstance){
+    			console.log('Contract is deployed at ' + newContractInstance.options.address);
+				//Use Add.at("the address")
+				//to retrieve the instance.
+				//Note, the at() method is provided from truffle console.
+			});
 	});
-}
+};
